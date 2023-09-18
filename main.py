cc636b5f21b086693b9b428782df90cd925df781
Škop Ondřej <oskop@novavax.com>
2023-09-18 16:48:22 +0200

    update del db

--- main.py
+++ main.py
@@ -3,11 +3,10 @@ import openai
 import streamlit as st
 with st.sidebar:
     openai_api_key = st.secrets["openai_key"]
+    st.title("OpenAI Playground")
 
-    "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
 
-
-st.title("💬 Chatbor")
+st.title("💬 Chatbot")
 
 if "messages" not in st.session_state:
     st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
@@ -16,6 +15,7 @@ for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
 
 if prompt := st.chat_input():
+
     if not openai_api_key:
         st.info("Please add your OpenAI API key to continue.")
         st.stop()
